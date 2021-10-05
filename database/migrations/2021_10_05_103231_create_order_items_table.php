a4b734c8f0a99f96083ecf356e8fbed28e8be53d
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-05 14:05:39 +0330

    create order_items migration

---
+++ database/migrations/2021_10_05_103231_create_order_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("price");
+            $table->unsignedInteger("quantity");
+            $table->unsignedInteger("subtotal");
+
+
+            $table->foreignId("order_id");
+            $table->foreign("order_id")->references("id")->on("orders")->cascadeOnDelete();
+
+            $table->foreignId("product_variation_id");
+            $table->foreign("product_variation_id")->references("id")->on("product_variations")->cascadeOnDelete();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
