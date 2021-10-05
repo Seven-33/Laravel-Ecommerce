a8b32e8d16ec98c0c7619ce8885ea0932eedbbfb
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-05 14:44:18 +0330

    create contact_us migration

---
+++ database/migrations/2021_10_05_111150_create_contact_us_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactUsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_us', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("name")->default("ناشناس");
+            $table->string("email")->nullable();
+            $table->string("subject")->nullable();
+            $table->text("text");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_us');
+    }
+}
