4da2dde3334b21b0b26de3d2cf5ae61c266fced1
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-07 23:58:31 +0330

    create brands route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BrandController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/admin-panel/dashboard', function () {
     return view('admin.dashboard');
+})->name("dashboard");
+
+Route::prefix("admin-panel/management")->name("admin.")->group(function(){
+
+    Route::resource("brands",BrandController::class);
 });
