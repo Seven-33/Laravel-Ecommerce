aafbbba2bb31e13d10298598cf8b5d5b6fa14406
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-08 00:09:40 +0330

    create Brand model

---
+++ app/Models/Brand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Brand extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $table = "brands";
+    protected $guarded = [];
+
+        /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
