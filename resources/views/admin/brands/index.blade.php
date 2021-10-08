5afbb13b046dbe4dcd0b013c0aebb44ebbe49440
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-08 12:10:52 +0330

    update brands index page

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -1,5 +1,49 @@
 @extends('admin.layouts.admin')
 
-@section('content')
-<h2>index page</h2>
+@section('title')
+    index brands
 @endsection
+
+@section('content')
+
+    <!-- Content Row -->
+    <div class="row">
+
+        <div class="col-xl-12 col-md-12 mb-4 p-md-5 bg-white">
+            <div class="d-flex justify-content-between mb-4">
+                <h5 class="font-weight-bold">لیست برند ها ({{ $brands->total() }})</h5>
+                <a class="btn btn-sm btn-outline-primary" href="{{ route('admin.brands.create') }}">
+                    <i class="fa fa-plus"></i>
+                    ایجاد برند
+                </a>
+            </div>
+
+            <div>
+                <table class="table table-bordered table-striped text-center">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>نام</th>
+                            <th>وضعیت</th>
+                            <th>عملیات</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($brands as $key => $brand)
+                            <tr>
+                                <th>{{ $brands->firstItem() + $key }}</th>
+                                <th>{{ $brand->name }}</th>
+                                <th>
+                                    <span class="{{$brand->getRawOriginal('is_active') ? "text-success" : "text-danger"}}">
+                                        {{ $brand->is_active }}
+                                    </span>
+                                </th>
+                                <th>#</th>
+                            </tr>
+
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endsection
