80d0ae3cc539ea607841c82e22a09c874b331a80
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-08 20:22:52 +0330

    create Attribute model

---
+++ app/Models/Attribute.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    use HasFactory;
+
+    protected $table = "attributes";
+    protected $guarded = [];
+}
