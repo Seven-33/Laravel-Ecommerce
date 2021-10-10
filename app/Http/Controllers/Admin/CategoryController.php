8e56ade246bcee8894ffaee39c964515b170ddf5
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-10 21:26:47 +0330

    store category using transaction

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Attribute;
 use App\Models\Category;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -26,10 +28,10 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        $parentCategories = Category::where("parent_id",0)->get();
+        $parentCategories = Category::where("parent_id", 0)->get();
         $attributes = Attribute::all();
 
-        return view("admin.categories.create", compact("parentCategories","attributes"));
+        return view("admin.categories.create", compact("parentCategories", "attributes"));
     }
 
     /**
@@ -40,7 +42,46 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
+            'parent_id' => 'required',
+            'attributes_ids' => 'required',
+            'attributes_is_filter_ids' => 'required',
+            'variation_id' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            $category =  Category::create([
+                'name' => $request->name,
+                'slug' => $request->slug,
+                'parent_id' => $request->parent_id,
+                'icon' => $request->icon,
+                'description' => $request->description,
+            ]);
+
+            foreach ($request->attributes_ids as $attributeId) {
+                $attribute = Attribute::findOrFail($attributeId);
+                $attribute->categories()->attach($category->id, [
+                    'is_filter' => in_array($attributeId, $request->attributes_is_filter_ids) ? 1 : 0,
+                    'is_variation' => $request->variation_id == $attributeId ? 1 : 0,
+
+                ]);
+            }
+
+            DB::commit();
+        } catch (Exception $ex) {
+            DB::rollBack();
+
+            alert()->success('مشکل در ایجاد دسته بندی', 'هشدار ')->persistent('حله');
+            return redirect()->back();
+        }
+
+        alert()->success('دسته بندی مورد نظر ایجاد شد', 'با تشکر');
+
+        return redirect()->route("admin.categories.index");
     }
 
     /**
