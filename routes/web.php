0d7b5d0b6758cd14d7b1084c07766f1cfdbda659
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-15 12:48:18 +0330

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,10 @@ use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+// temporarily redirect / route to dashboard
+Route::redirect('/', '/admin-panel/dashboard');
 
 Route::get('/admin-panel/dashboard', function () {
     return view('admin.dashboard');
