f66db2c318b7591f2cf1d52ea842b59677fef2eb
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-15 19:37:06 +0330

    add links to sidebar

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -42,7 +42,9 @@
         </a>
         <div id="collapseProducts" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar" style="">
           <div class="bg-white py-2 collapse-inner rounded">
+              <a class="collapse-item" href="{{route("admin.categories.index")}}">دسته بندی ها</a>
             <a class="collapse-item" href="{{route("admin.attributes.index")}}">ویژگی ها</a>
+            <a class="collapse-item" href="{{route("admin.tags.index")}}">تگ ها</a>
           </div>
         </div>
       </li>
