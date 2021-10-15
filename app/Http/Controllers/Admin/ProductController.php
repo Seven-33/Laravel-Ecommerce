53508d37debb56212444a5adc4960b836acfa5d6
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-15 21:58:10 +0330

    call create product in controller

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -24,7 +27,11 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $brands = Brand::all();
+        $tags = Tag::all();
+        $categories = Category::where('parent_id', '!=', 0)->get();
+
+        return view("admin.products.create", compact('brands','tags','categories'));
     }
 
     /**
