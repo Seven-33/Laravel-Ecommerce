8460acd60a3863ecc0cca381020b911ff34f0b48
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-15 21:00:06 +0330

    create product route && model && controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,6 @@ Route::prefix("admin-panel/management")->name("admin.")->group(function(){
     Route::resource("attributes",AttributeController::class);
     Route::resource("categories",CategoryController::class);
     Route::resource("tags",TagController::class);
+    Route::resource("products",ProductController::class);
+    
 });
