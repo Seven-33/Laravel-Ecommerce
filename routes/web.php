ea76e1f77cd8efefbcfa969d55f99d208b6de906
robinsondev428 <princerobinson428withmail@gmail.com>
2021-10-15 19:10:55 +0330

    create tag route && model && controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\BrandController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -19,4 +20,5 @@ Route::prefix("admin-panel/management")->name("admin.")->group(function(){
     Route::resource("brands",BrandController::class);
     Route::resource("attributes",AttributeController::class);
     Route::resource("categories",CategoryController::class);
+    Route::resource("tags",TagController::class);
 });
